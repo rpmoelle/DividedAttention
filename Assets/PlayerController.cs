ad0cf492410d69e54897176eed9f08d14f9eecbe
Rachel Moeller <parrishwork@gmail.com>
2017-10-26 14:22:19 -0400

    Forward Walking in Cam Direction
    No side movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,17 +14,49 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
         //Handle player movement
-        //Use vector forces and movement axis for possible controller support
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime;
 
-      //  gameObject.transform.Rotate(0, x, 0);
-    //Change this so forward is the direction of the camera
+        float h = 1f * Input.GetAxis("Mouse X");
+        float v = 2f * Input.GetAxis("Mouse Y");
 
-        float h = 1f * Input.GetAxis("Mouse X");//Change this value
-        //float v = 2f * Input.GetAxis("Mouse Y");
         cam.transform.Rotate(0, h, 0);
-        gameObject.transform.Translate(h, 0, z);
+        gameObject.transform.Rotate(0, h, 0);
+        gameObject.transform.position += z * cam.transform.forward;
+
+
+        /* 
+        gameObject.transform.Translate(0, 0, z);
+        gameObject.transform.Rotate(0, x, 0);
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
+       
+        cam.transform.Rotate(0, h, 0);
+
+        // create a ray going into the scene from the screen location the user clicked at
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // the raycast hit info will be filled by the Physics.Raycast() call further
+        RaycastHit hit;
+
+        // perform a raycast using our new ray. 
+        // If the ray collides with something solid in the scene, the "hit" structure will
+        // be filled with collision information
+        if (Physics.Raycast(ray, out hit))
+        {
+            // a collision occured. Check if it's our plane object and create our cube at the
+            // collision point, facing toward the collision normal
+            //if (hit.collider)
+              //  Debug.Log("Here");
+        }
+        /*
+         gameObject.transform.Rotate(0, x, 0);
+          //Change this so forward is the direction of the camera
+
+          float h = 1f * Input.GetAxis("Mouse X");
+          //float v = 2f * Input.GetAxis("Mouse Y");
+          cam.transform.Rotate(0, h, 0);
+          gameObject.transform.Translate(0, 0, z);*/
 
 
 
