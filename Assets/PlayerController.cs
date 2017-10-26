f8bc585ef8b4450c305ae88442759370e06e44d6
Rachel Moeller <parrishwork@gmail.com>
2017-10-26 14:42:50 -0400

    Raycasting From Center of Screen
    Can detect what collider the ray hits.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public GameObject cam; //This is the main camera that is a child of the player
     float speed;//Value to change walking speed
+    public BoxCollider buttonCol; //the collider on the button
 
 	// Use this for initialization
 	void Start () {
@@ -16,15 +17,16 @@ public class PlayerController : MonoBehaviour {
 
         //Handle player movement
         //Get player's input for what direction they want to go in
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float z = Input.GetAxis("Vertical") * Time.deltaTime;
 
         //Get where the mouse is pointing
         float h = 1f * Input.GetAxis("Mouse X");
         float v = 2f * Input.GetAxis("Mouse Y");
 
-        //Rotate the camera based on the player's rotation
+        //Rotate the camera based on the player's rotation and rotate the player object
         cam.transform.Rotate(0, h, 0);
+        //cam.transform.Rotate(v, 0, 0);//test
         gameObject.transform.Rotate(0, h, 0);
 
         //Move the player in the direction the camera is facing
@@ -34,10 +36,13 @@ public class PlayerController : MonoBehaviour {
         gameObject.transform.position += new Vector3(x, 0, 0);
 
 
-       
+        //Check to see if the player is looking at something interactable
 
         // create a ray going into the scene from the screen location the user clicked at
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Ray ray = cam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red);
 
         // the raycast hit info will be filled by the Physics.Raycast() call further
         RaycastHit hit;
@@ -45,12 +50,13 @@ public class PlayerController : MonoBehaviour {
         // perform a raycast using our new ray. 
         // If the ray collides with something solid in the scene, the "hit" structure will
         // be filled with collision information
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, 10f))
         {
             // a collision occured. Check if it's our plane object and create our cube at the
             // collision point, facing toward the collision normal
-            //if (hit.collider)
-              //  Debug.Log("Here");
+            if (hit.collider == buttonCol) {
+                Debug.Log("Here");
+            }
         }
        
 
